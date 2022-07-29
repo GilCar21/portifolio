7e5a187ab2548b6b9ad0cba67e235a6f5c96c3c0
Gilberto <gilbertocarvalho1997@gmil.com>
2022-07-29 14:13:39 -0300

    atualização de pequenos ajustes

--- src/components/Fooder/index.jsx
+++ src/components/Fooder/index.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa'
-import { MdOutlineMail } from 'react-icons/md'
-import { SiWhatsapp } from 'react-icons/si'
 
 import './fooder.css'
 
@@ -9,27 +6,7 @@ export function Fooder() {
     return (
         <div className="fooder">
             <div className='containerFooder'>
-                <div>
-                    <a href="mailto:gilbertocarvalho1997" target="_blank">
-                        <MdOutlineMail className='contact_icon' />
-                    </a>
-                    <a href="https://wa.me/5584987969544" target="_blank">
-                        <SiWhatsapp className='contact_icon' />
-                    </a>
-
-                </div>
-                <p>Gilberto <br /> 2022</p>
-                <div className='redes'>
-                    <a href="https://github.com/GilCar21">
-                        <FaGithub className='icon' />
-                    </a>
-                    <a href="https://www.linkedin.com/in/gilbertojcneto/">
-                        <FaLinkedin className='icon' />
-                    </a>
-                    <a href="https://www.instagram.com/gilberto_carvalho21/">
-                        <FaInstagram className='icon' />
-                    </a>
-                </div>
+                <p>Gilberto Jorge<br /> 2022</p>
             </div>
         </div>
 
